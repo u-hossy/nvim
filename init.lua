4515c79fc98938df3bb6f18e540a05eb86bc0a27
u-hossy <95125392+u-hossy@users.noreply.github.com>
2024-06-04 23:35:34 +0900

    行番号を追加する設定などをした

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- 文字コードの指定
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+-- 列番号を表示
+vim.wo.number = true
+
+-- swapファイルを作成しない
+-- vim.opt.swapfile = false
