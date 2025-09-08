7440441b3b0d8ef39a3b9cb7c147d23ed268dee2
u-hossy <95125392+u-hossy@users.noreply.github.com>
2025-09-08 17:21:47 +0900

    add `telescope.nvim`

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,9 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
